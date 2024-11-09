f6ff8512d53eccc50b6e87e9e4c783d4c3cabaa1
CoolBreezeArcanine <1052611718@qq.com>
2024-11-10 04:45:57 +0800

    v1.0.0-alpha.2版本号发布配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "icu.fuck.blued"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.0-alpha.1"
+        versionCode = 3
+        versionName = "1.0.0-alpha.2"
     }
 
     buildTypes {
